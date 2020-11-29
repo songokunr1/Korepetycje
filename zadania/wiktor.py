6a26028ca53d002e54d4202e3c6c19706d36693a
songokunr1 <pawelm18@gmail.com>
2020-11-29 09:50:30 +0100

    Create wiktor.py

---
+++ zadania/wiktor.py
@@ -0,0 +1,22 @@
+a = 1234  #
+lista_liczb = [1, 2, 3, 100, -1]
+krotka_liczb_2 = (1, 2, 3, 100, -1)
+lista_liczb[0]
+# print(a)
+for liczba in krotka_liczb_2:
+    print(liczba)
+
+a = 10
+b = 20
+c = a + b
+
+class Lista():
+    a = []
+    def __init__(self, obiekt):
+        self.obiekt = obiekt
+
+
+    def append(self, obiekt):
+        asdf;aa
+        asdfasd
+lista_liczb.append()
