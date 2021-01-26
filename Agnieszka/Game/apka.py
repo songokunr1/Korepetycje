4aabb755fc0fc7d4b38d98e6cb4cdb4e0f131e1e
songokunr1 <pawelm18@gmail.com>
2021-01-26 21:38:08 +0100

    Create apka.py

---
+++ Agnieszka/Game/apka.py
@@ -0,0 +1,23 @@
+# #!flask/bin/python
+# # -*- coding: utf-8 -*-
+# from clp3 import clp
+#
+# from flask import Flask
+# from flask import jsonify
+# from flask import render_template
+# from flask import request
+# from flask import abort, redirect, url_for
+#
+# app = Flask(__name__)
+#
+# ##########################################################
+#
+# @app.route('/')
+# def index():
+#         return 'HELLO'
+#
+# ##########################################################
+#
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug=True, port=5121)
+#
